a69efad64b3897ef541d789dc588c5ea6772cdbc
Wojciech Sowiński <wojciech.sowinski@owliedev.pl>
2022-10-18 14:58:21 +0200

    config change

--- client/src/config.js
+++ client/src/config.js
@@ -1,9 +1,7 @@
 // import defaultImg from './img/blank-profile-picture.png'
 
 const config = {
-    // serverUrl: 'http://serv.owliedev.pl:80/'
     serverUrl: 'https://tinder-clone-enzv.vercel.app/'
-    // defaultUserImg: defaultImg
 };
 
 export default config
